faec59065d894c36dc09b9263bceee7e0e504db1
Map1e <3412883097@qq.com>
2026-01-16 22:09:42 +0800

    init

---
+++ src/main/java/org/example/multimedia_file_security/MultimediaFileSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.multimedia_file_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimediaFileSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultimediaFileSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/multimedia_file_security/MultimediaFileSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.multimedia_file_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimediaFileSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
